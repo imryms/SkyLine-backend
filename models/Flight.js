8cdf76462d674636dbb64a6af02d7aee112f9bbc
FatemaMh <Fatemabusharar@gmail.com>
2026-04-21 21:02:42 +0300

    Update Booking

--- models/Flight.js
+++ models/Flight.js
@@ -1,64 +1,75 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
-const flightSchema = new mongoose.Schema({
-  flightNum:{
-    type: String,
-    required: true},
+const flightSchema = new mongoose.Schema(
+  {
+    flightNum: {
+      type: String,
+      required: true,
+    },
 
-  airLineCode:{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'AirLine',
-    required: true
+    airLineCode: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "AirLine",
+      required: true,
+    },
+
+    departureAirport: {
+      type: String,
+      required: true,
+    },
+
+    arrivalAirport: {
+      type: String,
+      required: true,
+    },
+
+    flightDate: {
+      type: Date,
+      required: true,
+    },
+
+    departureTime: {
+      type: String,
+      required: true,
+    },
+
+    arrivalTime: {
+      type: String,
+      required: true,
+    },
+
+    price: {
+      firstClass: {
+        type: Number,
+        required: true,
+      },
+      business: {
+        type: Number,
+        required: true,
+      },
+      economy: {
+        type: Number,
+        required: true,
+      },
+    },
+
+    availableSeats: {
+      type: Number,
+      required: true,
+    },
+
+    duration: {
+      type: String,
+      required: true,
+    },
+
+    isDirect: {
+      type: Boolean,
+      required: true,
+    },
   },
+  { timestamps: true }
+)
 
-  departureAirport:{
-    type: String,
-    required: true},
-
-  arrivalAirport:{
-    type: String,
-    required: true},
-
-  flightDate:{
-    type: Date,
-    required: true},
-
-  departureTime:{
-    type: String,
-    required: true},
-
-  arrivalTime:{
-    type: String,
-    required: true},
-
-  price:{
-    firstClass:{
-      type: Number,
-      required: true
-    },
-    business:{
-      type: Number,
-    required: true
-    },
-    economy:{
-      type: Number,
-      required: true
-    }
-    },
-
-  availableSeats:{
-    type: Number,
-    required: true},
-
-  duration:{
-    type: String,
-    required: true},
-
-  isDirect:{
-    type: Boolean,
-    required: true}
-
-},{timestamps: true});
-
-const Flight = mongoose.model('Flight', flightSchema)
+const Flight = mongoose.model("Flight", flightSchema)
 module.exports = Flight
